7ff5bdebd84103cc60a93c6f95271b9b15a6ca13
Adeyinka Junaid <yinkajunn@gmail.com>
2024-05-03 19:36:49 +0100

    Grid layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,210 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import timeData from '../data.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+  const [timeFrame, setTimeFrame] = useState('Daily')
+  const [previousTimePeriod, setPreviousTimePeriod] = useState('Yesterday')
+
+
+  useEffect(() => {
+    setData(timeData)
+    console.log(data)
+  }, [data])
+
+  const changeTimeFrame = (val) => {
+    console.log(val)
+    setTimeFrame(val)
+    if (val !== previousTimePeriod) {
+      if (val === 'Daily') {
+        setPreviousTimePeriod('Yesterday');
+      } else if (val === 'Weekly') {
+        setPreviousTimePeriod('Last Week');
+      } else if (val === 'Monthly') {
+        setPreviousTimePeriod('Last Month');
+      }
+    }
+  }
+  // {{ timeframe === 'Daily' ? 'active bg-indigo-500 text-white' : '' }}
 
   return (
-    <>
-  Report for
-  Jeremy Robson
+    <div className='flex'>
+      <h1>{timeFrame}</h1>
+      <section className="border-2 border-gray-300 mr-4 bg-[#5847eb">
+        <p>Report for</p>
+        <h1>Jeremy Robson</h1>
+        <ul className='list-none'>
+          <li><button onClick={() => changeTimeFrame('Daily')}>Daily</button></li>
+          <li><button onClick={() => changeTimeFrame('Weekly')}>Weekly</button></li>
+          <li><button onClick={() => changeTimeFrame('Monthly')}>Monthly</button></li>
+        </ul>
+      </section>
 
-  Daily
-  Weekly
-  Monthly
+      <section className="border-2 border-gray-300 grid grid-cols-3 gap-4">
 
-  Work
-  5hrs <!-- daily -->
-  Previous - 7hrs <!-- daily -->
-  32hrs <!-- weekly -->
-  Previous - 36hrs <!-- weekly -->
-  103hrs <!-- monthly -->
-  Previous - 128hrs <!-- monthly -->
+        <div className="rounded-md border-2 border-gray-800">
+          <h4>
+            Work
+          </h4>
 
-  Play
-  1hr <!-- daily -->
-  Previous - 2hrs <!-- daily -->
-  10hrs <!-- weekly -->
-  Previous - 8hrs <!-- weekly -->
-  23hrs <!-- monthly -->
-  Previous - 29hrs <!-- monthly -->
+          <p>
+            {data && (
+              <>
+              <h1>hii</h1>
+                {data[0].timeframes[`${timeFrame.toLocaleLowerCase().current}`]}
+              </>
+            )}
+          </p>
+          <p>
+            {previousTimePeriod} - 7hrs
+          </p>
 
-  Study
-  0hrs <!-- daily -->
-  Previous - 1hr <!-- daily -->
-  4hrs <!-- weekly -->
-  Previous - 7hrs <!-- weekly -->
-  13hrs <!-- monthly -->
-  Previous - 19hrs <!-- monthly -->
+          {/* <p>
+            32hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 36hrs
+          </p>
 
-  Exercise
-  1hr <!-- daily -->
-  Previous - 1hr <!-- daily -->
-  4hrs <!-- weekly -->
-  Previous - 5hrs <!-- weekly -->
-  11hrs <!-- monthly -->
-  Previous - 18hrs <!-- monthly -->
+          <p>
+            103hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 128hrs
+          </p> */}
 
-  Social
-  1hr <!-- daily -->
-  Previous - 3hrs <!-- daily -->
-  5hrs <!-- weekly -->
-  Previous - 10hrs <!-- weekly -->
-  21hrs <!-- monthly -->
-  Previous - 23hrs <!-- monthly -->
+        </div>
 
-  Self Care
-  0hrs <!-- daily -->
-  Previous - 1hr <!-- daily -->
-  2hrs <!-- weekly -->
-  Previous - 2hrs <!-- weekly -->
-  7hrs <!-- monthly -->
-  Previous - 11hrs <!-- monthly -->
+        <div className="rounded-md border-2 border-gray-800">
+          <h4>
+            Play
+          </h4>
+          <p>
+            1hr
+          </p>
+          <p>
+            {previousTimePeriod} - 2hrs
+          </p>
+          <p>
+            10hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 8hrs
+          </p>
+          <p>
+            23hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 29hrs
+          </p>
+        </div>
 
-  
-  <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
-  </div>
-    </>
+        <div className="rounded-md border-2 border-gray-800">
+          <h4>
+            Study
+          </h4>
+          <p>
+            0hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 1hr
+          </p>
+          <p>
+            4hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 7hrs
+          </p>
+          <p>
+            13hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 19hrs
+          </p>
+        </div>
+
+        <div className="rounded-md border-2 border-gray-800">
+          <h4>
+            Exercise
+          </h4>
+          <p>
+            1hr
+          </p>
+          <p>
+            {previousTimePeriod} - 1hr
+          </p>
+          <p>
+            4hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 5hrs
+          </p>
+          <p>
+            11hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 18hrs
+          </p>
+        </div>
+
+        <div className="rounded-md border-2 border-gray-800">
+          <h4>
+            Social
+          </h4>
+          <p>
+            1hr
+          </p>
+          <p>
+            {previousTimePeriod} - 3hrs
+          </p>
+          <p>
+            5hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 10hrs
+          </p>
+          <p>
+            21hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 23hrs
+          </p>
+        </div>
+
+        <div className="rounded-md border-2 border-gray-800">
+          <h4>
+            Self Care
+          </h4>
+          <p>
+            0hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 1hr
+          </p>
+          <p>
+            2hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 2hrs
+          </p>
+          <p>
+            7hrs
+          </p>
+          <p>
+            {previousTimePeriod} - 11hrs
+          </p>
+        </div>
+      </section>
+
+
+
+
+      {/* <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
+      </div> */}
+    </div>
   )
 }
 
